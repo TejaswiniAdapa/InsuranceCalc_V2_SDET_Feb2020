fd9d8105533efa9af9632f177dbb50148c0f4d0c
Adapa Teja <tejaswini1678@gmail.com>
2020-03-25 01:24:55 -0400

    March 25 commit

--- src/main/java/SeleniumPractice_Assignments/Assignment4.java
+++ src/main/java/SeleniumPractice_Assignments/Assignment4.java
@@ -26,7 +26,7 @@ public class Assignment4 {
 	WebDriver driver = null;
 
 	// This Method executes before all other methods gets executed
-	@Test(priority=1)
+	@Test(priority = 1)
 	public void Browsersetup() {
 		WebDriverManager.chromedriver().setup();
 
@@ -53,7 +53,7 @@ public class Assignment4 {
 
 		String Dashboard = driver.getTitle();
 		Assert.assertEquals(Dashboard, "qdPM | Dashboard");
-		System.out.println("------------Login Sucessfull--------------"+"/n");
+		System.out.println("------------Login Sucessfull--------------" + "/n");
 	}
 
 	/*
@@ -71,18 +71,18 @@ public class Assignment4 {
 			String element_text = d_options.getText();
 
 			Assert.assertTrue(true, element_text);
-			
+
 			System.out.println(element_text);
 		}
 		System.out.println();
-		
+
 	}
 
 	/*
 	 * //Test case 3: Verify the page title after the logout action TEST STEPS-
-	 * 1.Click on Add task button and select Test project 1 option 
-	 * 2.Enter Required details for all the tabs-General,Time,attachments
-	 * 3.Goto admin option and logout from application
+	 * 1.Click on Add task button and select Test project 1 option 2.Enter Required
+	 * details for all the tabs-General,Time,attachments 3.Goto admin option and
+	 * logout from application
 	 */
 	@Test(priority = 4)
 	public void Addtask() throws InterruptedException, AWTException {
@@ -98,7 +98,7 @@ public class Assignment4 {
 		Type_option.selectByVisibleText("Defects (Hourly rate $0.00)");
 
 		WebElement name = driver.findElement(By.id("tasks_name"));
-		name.sendKeys("Teja");
+		name.sendKeys("TestUser");
 
 		WebElement Status = driver.findElement(By.id("tasks_tasks_status_id"));
 		Select Status_option = new Select(Status);
@@ -111,66 +111,100 @@ public class Assignment4 {
 		WebElement label = driver.findElement(By.id("tasks_tasks_label_id"));
 		Select label_option = new Select(label);
 		label_option.selectByVisibleText("Issue");
-		
+
 		driver.findElement(By.xpath("//div[@id='assigned_to']//input[@value='2']")).click();
 		driver.findElement(By.xpath("//div[@id='assigned_to']//input[@value='4']")).click();
 
-		WebElement createdBy= driver.findElement(By.id("tasks_created_by"));
-		Select createdBy_option=new Select(createdBy);
+		WebElement createdBy = driver.findElement(By.id("tasks_created_by"));
+		Select createdBy_option = new Select(createdBy);
 		createdBy_option.selectByValue("4");
-		
-		WebElement Time_Task=driver.findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/div/ul/li[2]/a"));
-		//To Hover my mouse we use Actions class Actions act=new
-		Actions act=new Actions(driver); 
+
+		WebElement Time_Task = driver.findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/div/ul/li[2]/a"));
+		// To Hover my mouse we use Actions class Actions act=new
+		Actions act = new Actions(driver);
 		act.moveToElement(Time_Task).click().build().perform();
-		 
+
 		driver.findElement(By.id("tasks_estimated_time")).sendKeys("11");
 		driver.findElement(By.id("tasks_start_date")).sendKeys("2019-12-27");
 		driver.findElement(By.id("tasks_due_date")).sendKeys("tasks_due_date");
-		 
-		WebElement progress_task=driver.findElement(By.id("tasks_progress"));
-		Select progress_task_options=new Select(progress_task);
+
+		WebElement progress_task = driver.findElement(By.id("tasks_progress"));
+		Select progress_task_options = new Select(progress_task);
 		progress_task_options.selectByValue("20");
-		 
-		WebElement Attachement_task=driver.findElement(By.xpath( "//*[@id=\"tasks\"]/div[1]/div/ul/li[3]/a"));
+
+		WebElement Attachement_task = driver.findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/div/ul/li[3]/a"));
 		act.moveToElement(Attachement_task).click().build().perform();
+
 		
-		//didnot work with sendkeys
-		//upload.sendKeys("C:\\\\Users\\\\Adapa Teja\\\\OneDrive\\\\Pictures\\\\Screenshots\\\\Screenshot (1)"); 	
+		// The following code is by using Robot class to upload picture
+		driver.findElement(By.xpath("//*[@id=\"uploadifive-uploadify_file_upload\"]")).click();
 		
-		//The following code is by using Robot class to upload picture		
-		driver.findElement(By.xpath("//*[@id=\"uploadifive-uploadify_file_upload\"]")).click();;
-		
-		//copy the path to clipboard
-				String filepath="C:\\Users\\Adapa Teja\\OneDrive\\Pictures\\Screenshots\\Fail_testcase";
-				StringSelection strsel=new StringSelection(filepath);
-				Toolkit toolkit=Toolkit.getDefaultToolkit();
-				Clipboard clipboard = toolkit.getSystemClipboard();
-				clipboard.setContents(strsel, null);
-				
-				//use robot class to CTRL+V
-				Robot rb=new Robot();
-				rb.keyPress(KeyEvent.VK_CONTROL);
-				rb.keyPress(KeyEvent.VK_V);
-				Thread.sleep(2000);
-				rb.keyRelease(KeyEvent.VK_CONTROL);
-				rb.keyRelease(KeyEvent.VK_V);
-				
-				//use robot class Hit enter key
-				rb.keyPress(KeyEvent.VK_ENTER);
-				rb.keyRelease(KeyEvent.VK_ENTER);
-			Thread.sleep(15000);
+
+		// copy the path to clipboard
+		String filepath = "C:\\Users\\Adapa Teja\\OneDrive\\Pictures\\Screenshots\\Fail_testcase";
+		StringSelection strsel = new StringSelection(filepath);
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Clipboard clipboard = toolkit.getSystemClipboard();
+		clipboard.setContents(strsel, null);
+
+		// use robot class to CTRL+V
+		Robot rb = new Robot();
+		rb.keyPress(KeyEvent.VK_CONTROL);
+		rb.keyPress(KeyEvent.VK_V);
+		Thread.sleep(2000);
+		rb.keyRelease(KeyEvent.VK_CONTROL);
+		rb.keyRelease(KeyEvent.VK_V);
+
+		// use robot class Hit enter key
+		rb.keyPress(KeyEvent.VK_ENTER);
+		rb.keyRelease(KeyEvent.VK_ENTER);
+		Thread.sleep(15000);
 		driver.findElement(By.xpath("/*//form[@id='tasks']/div[2]/button[1]")).click();
 		driver.findElement(By.xpath("//*[@id=\"yahoo-com\"]/div[1]/div/ul/li/a/span")).click();
-		String valueToBeSelected=" Logoff";
-		driver.findElement(By.xpath("//*[@id=\"yahoo-com\"]/div[1]/div/ul/li/ul//a[text()='"+valueToBeSelected+"']")).click();
+		String valueToBeSelected = " Logoff";
+		driver.findElement(By.xpath("//*[@id=\"yahoo-com\"]/div[1]/div/ul/li/ul//a[text()='" + valueToBeSelected + "']")).click();
 		
-		String TitleAfterLogout=driver.getTitle();
+		//Verify the title of login page
+		String TitleAfterLogout = driver.getTitle();
 		Assert.assertEquals(TitleAfterLogout, "qdPM | Login");
 	}
 
+	@Test(priority = 5)
+	public void Searchbox() throws Exception
+	{
+		driver.findElement(By.name("login[email]")).sendKeys("administrator@localhost.com");
+		driver.findElement(By.name("login[password]")).sendKeys("administrator");
+		driver.findElement(By.xpath("//button[@class='btn btn-info pull-right']")).click();
+		
+		//Click on Tasks option from Dashboard options
+		driver.findElement(By.xpath("//*[@id=\"yahoo-com\"]/div[3]/div[1]/div/div/ul/li[4]/a")).click();
+		
+		//selecting the View All option
+		String Task_dd="View All";
+		driver.findElement(By.xpath("//*[@id='yahoo-com']/div[3]/div[1]/div/div/ul/li[4]/ul//span[text()='"+Task_dd+"']")).click();
+		
+		//Selecting the Search box by using mouse actions
+		WebElement search=driver.findElement(By.xpath("//*[@id=\"yui-gen1\"]/span"));
+		Actions act1 = new Actions(driver);
+		act1.moveToElement(search).click().build().perform();
+		
+		Thread.sleep(5000);
+		//Searching for the created Tasks by name entered='Teja'
+		driver.findElement(By.id("search_keywords")).sendKeys("TestUser");
+		driver.findElement(By.xpath("//input[@class='btn btn-default']")).click();
+		
+		//Verify the search result i.e test data against what i have given for 'Add Task' Method
+		
+		String Name="TestUser";
+	WebElement actual_output=driver.findElement(By.xpath("//table[@id='itmes_listing_1990109']/tbody/tr//a[text()='"+Name+"']"));
+	Assert.assertEquals(actual_output, "TestUser");
+	}
+	
+	
+	
+
 	// This method executes after all methods gets executed
-	@Test(priority=5)
+	@Test(priority = 6)
 	public void BrowserClose() {
 
 		driver.close();
